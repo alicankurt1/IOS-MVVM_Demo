7d73f3fe8e346e70d8f134ce8e6ecdbf5a20a142
Alican Kurt <kurta15@itu.edu.tr>
2022-01-11 12:36:48 +0300

    Initial Commit

---
+++ MVVM_Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_Demo
+//
+//  Created by Alican Kurt on 11.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
